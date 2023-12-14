5409a209a376535350ea0c5d7b1511ee2251e0f2
TheFutri <smfurdyna@gmail.com>
2023-12-14 16:47:11 +0100

    game logic added

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let pcScore = 0
+let playerScore = 0
+let result = 0
+
 function getComputerChoice() { 
     let number = Math.floor(Math.random() * 3)
     if(number === 0) {
@@ -20,10 +24,12 @@ function playRound(playerSelection, computerSelection) {
     (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') ||
     (playerSelection === 'SCISSORS' && computerSelection === 'PAPER'))
     {
-        return `You Win! ${playerSelection} beats ${computerSelection}` 
+        playerScore++
+        return `You Win! ${playerSelection} ${playerScore} beats ${computerSelection} ${pcScore}` 
     }
     else {
-        return `You Lose! ${computerSelection} beats ${playerSelection}`
+        pcScore++
+        return `You Lose! ${computerSelection} ${playerScore} beats ${playerSelection} ${pcScore}`
     }
 }
 
@@ -32,19 +38,31 @@ function game() {
         const playerSelection = 'ROCK'
         const computerSelection = getComputerChoice()
         console.log(playRound(playerSelection, computerSelection))
+        whoWin(pcScore, playerScore)
     })
 
     document.getElementById('paper').addEventListener('click', () => {
         const playerSelection = 'PAPER'
         const computerSelection = getComputerChoice()
         console.log(playRound(playerSelection, computerSelection))
+        whoWin(pcScore, playerScore)
     })
 
     document.getElementById('scissors').addEventListener('click', () => {
         const playerSelection = 'SCISSORS'
         const computerSelection = getComputerChoice()
         console.log(playRound(playerSelection, computerSelection))
+        whoWin(pcScore, playerScore)
     })
+}
+
+function whoWin(pcScore, playerScore) {
+    if(pcScore === 5) {
+        return `Player win ${playerScore} - ${pcScore}`
+    }
+    else {
+        return `PC win ${playerScore} - ${pcScore}`
+    }
 }
 
 
